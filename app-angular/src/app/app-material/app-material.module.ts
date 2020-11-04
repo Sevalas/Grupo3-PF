ef0526010ff641b68fefa461b75f1c160446ecbe
Sevalas <se.valas@outlook.com>
2020-11-04 13:17:54 -0300

    nada

--- app-angular/src/app/app-material/app-material.module.ts
+++ app-angular/src/app/app-material/app-material.module.ts
@@ -1,12 +1,13 @@
+import {NgModule} from '@angular/core';
 import { MatButtonModule } from'@angular/material/button';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule} from '@angular/common';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
 import {MatGridListModule} from '@angular/material/grid-list';
-
-
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [],
@@ -19,6 +20,9 @@ import {MatGridListModule} from '@angular/material/grid-list';
     MatInputModule,
     MatIconModule,
     MatGridListModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule
   ]
 })
 export class AppMaterialModule { }
