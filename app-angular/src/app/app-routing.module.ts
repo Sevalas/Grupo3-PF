88349f22f944b1b49be09c1a4b8fd87912ca6dd2
Sevalas <se.valas@outlook.com>
2020-11-04 21:41:47 -0300

    nabvar

--- app-angular/src/app/app-routing.module.ts
+++ app-angular/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { PetExpoComponent } from './components/pet-expo/pet-expo.component';
 import { RegistroComponent } from './components/registro/registro.component';
 
 const routes: Routes = [
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'registro',
     component: RegistroComponent
+  },
+  {
+    path: 'tablero',
+    component: PetExpoComponent
   }
 ];
 
--- app-angular/src/app/components/login/login.component.ts
+++ app-angular/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  user:string;
+  pass:string;
+
+  test(){
+    if(this.user === 'admin' && this.pass === 'admin'){
+      this.router.navigate(['/tablero']);
+    }
+  }
 }
