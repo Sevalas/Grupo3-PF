30792e9af5a38f668017df2aa461a6c3fdf9cbcb
Sevalas <se.valas@outlook.com>
2020-11-11 10:11:52 -0300

    spring

---
+++ adopcionPF/src/main/java/grupo3/adopcionPF/AdopcionPfApplication.java
@@ -0,0 +1,13 @@
+package grupo3.adopcionPF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdopcionPfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdopcionPfApplication.class, args);
+	}
+
+}
---
+++ adopcionPF/src/test/java/grupo3/adopcionPF/AdopcionPfApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo3.adopcionPF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdopcionPfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
